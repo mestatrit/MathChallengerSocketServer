f16e6b78d949bb56a8748c4698cf8cb2c0fcbd12
pinoelefante <pinoelefante@hotmail.it>
2014-05-24 12:52:45 +0200

    generatore espressioni medie -> generatore espressioni difficili

--- src/it/mathchallenger/server/controls/GeneratoreEspressioniDifficili.java
+++ src/it/mathchallenger/server/controls/GeneratoreEspressioniDifficili.java
@@ -2,16 +2,35 @@ package it.mathchallenger.server.controls;
 
 import java.util.ArrayList;
 
+import sun.security.jca.GetInstance;
 import it.mathchallenger.server.entities.Domanda;
 import de.congrace.exp4j.UnknownFunctionException;
 import de.congrace.exp4j.UnparsableExpressionException;
 
-public class GeneratoreEspressioniMedie extends Risolutore {
-
+public class GeneratoreEspressioniDifficili extends Risolutore {
+	private static GeneratoreEspressioniDifficili ris;
+	public static GeneratoreEspressioniDifficili getInstance(){
+		if(ris==null)
+			ris=new GeneratoreEspressioniDifficili();
+		return ris;
+	}
+	private GeneratoreEspressioniDifficili(){
+		super();
+	}
 	@Override
 	public Domanda generaDomanda() {
-		// TODO Auto-generated method stub
-		return null;
+		StringBuilder domanda=new StringBuilder("(");
+		domanda.append(generaEasy());
+		domanda.append(")");
+		String op=operazioni_easy[rand.nextInt(operazioni_easy.length-1)];
+		domanda.append(op);
+		domanda.append("(");
+		domanda.append(generaEasy());
+		domanda.append(")");
+		
+		Domanda dom=new Domanda();
+		dom.setDomanda(domanda.toString());
+		return dom;
 	}
 
 	@Override
@@ -31,15 +50,15 @@ public class GeneratoreEspressioniMedie extends Risolutore {
 		switch(op){
 			case "+":
 			case "-":
-				operatori[0]=rand.nextInt(101);
-				operatori[1]=rand.nextInt(50)+1;
+				operatori[0]=rand.nextInt(30)+1;
+				operatori[1]=rand.nextInt(30)+1;
 				break;
 			case "*":
-				operatori[0]=rand.nextInt(21);
+				operatori[0]=rand.nextInt(11);
 				operatori[1]=rand.nextInt(11);
 				break;
 			case "/":
-				operatori[0]=rand.nextInt(101);
+				operatori[0]=rand.nextInt(50)+1;
 				ArrayList<Integer> divisori=new ArrayList<Integer>();
 				operatori[1]=operatori[0]/2;
 				while(operatori[1]>1){
@@ -49,8 +68,6 @@ public class GeneratoreEspressioniMedie extends Risolutore {
 				}
 				if(divisori.size()==0){
 					divisori.add(1);
-					divisori.add(-operatori[0]);
-					divisori.add(-1);
 					divisori.add(operatori[0]);
 				}
 				operatori[1]=divisori.get(rand.nextInt(divisori.size()));
@@ -59,5 +76,14 @@ public class GeneratoreEspressioniMedie extends Risolutore {
 		if(rand.nextInt(10)<=2)
 			operatori[0]*=(-1);
 		return operatori[0]+op+operatori[1];
+	}
+	public static void main(String[] args){
+		Risolutore ris=getInstance();
+		int i=0;
+		while(i<1000){
+    		Domanda d=ris.generaDomanda();
+    		System.out.println(d.getDomanda());
+    		i++;
+		}
 	}
 }
